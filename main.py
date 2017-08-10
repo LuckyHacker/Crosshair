693a74850053fc8aedf86d88d5eccd932955dff1
Onni Hakkari <onni.hakkari@gmail.com>
2017-08-11 01:48:27 +0300

    Added console for configuring crosshair

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from crosshair import Crosshair
 import json
+import threading
 
-def main():
+def reload_crosshair(c = None):
+    if c:
+        c.allow_draw = False
+
     with open("config.json", "r") as f:
         config = json.loads(f.read())
 
@@ -13,7 +17,41 @@ def main():
 
     c.create_crosshair_matrix()
     c.save_crosshair_png()
-    c.draw_crosshair_pixels()
+    c_thread = threading.Thread(target=c.draw_crosshair_pixels)
+    c_thread.daemon = True
+    c_thread.start()
+    return c
+
+
+def update_config(key, value):
+    with open("config.json", "r") as f:
+        config = json.loads(f.read())
+
+    if key == "color":
+        config[key] = value
+    else:
+        config[key] = int(value)
+
+
+    with open("config.json", "w") as f:
+        f.write(json.dumps(config))
+
+def main():
+    c = reload_crosshair()
+    commands = ["thickness", "length", "offset", "color", "set_pixel_fps"]
+    command = ""
+    while command != "exit":
+        command = input("crosshair> ")
+
+        try:
+            key, value = command.split(" ")
+            if key in commands:
+                update_config(key, value)
+        except:
+            if command not in ("exit", ""):
+                print("Invalid command")
+
+        c = reload_crosshair(c)
 
 if __name__ == "__main__":
     main()
